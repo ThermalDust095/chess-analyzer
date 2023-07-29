48dfb587ef417fcd7abd72284bf6ff67a78935f1
John-C7 <johncharlesjt2003@gmail.com>
2023-07-29 15:04:10 +0530

    updated the code to fetch latest 3 games pgn files

--- fetchpgn.py
+++ fetchpgn.py
@@ -1,24 +1,25 @@
 import os
 import requests
-from datetime import date  
+from datetime import date
 
+#fetches 3 pgn of the latest 3 games played
 def main(user, where):
-    print("Downloading %s's games to %s:" % (user, where))
-    for archive in get('https://api.chess.com/pub/player/%s/games/archives' % user)['archives']:
-        download_archive(archive, where)
+    print("Downloading %s's last 3 games to %s:" % (user, where))
+    archives = get('https://api.chess.com/pub/player/%s/games/archives' % user)['archives']
+    latest_archives = archives[-3:]  # Retrieve the last three archives
 
-def download_archive(url, where):
-    games = get(url)['games']
-    d = date.fromtimestamp(games[0]['end_time'])
-    y = d.year
-    m = d.month
-    filename = os.path.join(where, "%d-%02d.pgn" % (y, m))
-    print('Starting work on %s...' % filename)
+    for idx, archive in enumerate(latest_archives, 1):
+        games = get(archive)['games']
+        latest_game = games[-1]  # Retrieve the last game (latest)
 
-    with open(filename, 'w', encoding='utf-8') as output:
-        for game in games:
-            print(game['pgn'], file=output)
-            print('', file=output)
+        d = date.fromtimestamp(latest_game['end_time'])
+        y = d.year
+        m = d.month
+        filename = os.path.join(where, "pgn%d-%d-%02d.pgn" % (idx, y, m))
+        print('Starting work on %s...' % filename)
+
+        with open(filename, 'w', encoding='utf-8') as output:
+            print(latest_game['pgn'], file=output)
 
 def get(url):
     return requests.get(url).json()
@@ -28,6 +29,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Download a user's games from chess.com")
     parser.add_argument('user', metavar='USER', help='The user whose games we want')
     parser.add_argument('where', metavar='PATH', help='Where to create the PGN files',
-            default=".", nargs='?')
+                        default=".", nargs='?')
     args = parser.parse_args()
     main(args.user, args.where)
