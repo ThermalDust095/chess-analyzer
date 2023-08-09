48305a0a3bc9c0132f555d8279a38245a1c92e86
DoubellBerserker <61311217+DoubellBerserker@users.noreply.github.com>
2023-08-10 03:11:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
-import pygame
-from pygame.locals import *
-import os
-import sys
-a = os.getcwd()
-sys.path.insert(0,rf'{a}\Analysis')
-from fetchpgn import *
-from userinfo import *
-sys.path.insert(0,rf'{a}\GUI')
-from gui_main import *
+import argparse
 
+from Analysis.analyser import *
+from Analysis.fetchpgn import *
+from Analysis.userinfo import *
+from GUI.gui_main import Main
 
-username = input("Give Your chess.com username: ")
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--username", help="enter your Chess.com username", type=str, required=True)
+# parser.add_argument("-g", "--game", help="enter the game to select", type=int, required=True)
+args = parser.parse_args()
 
-get_player_rating(username)
+print("\n----------Rating Stats of {user}----------".format(user=args.username))
+ratings = get_player_rating(args.username)
 
+print("Enter number of games to display [MAX = 5]: ", end='')
+games = int(input())
+print('')
 
+time_control = fetchpgn(args.username, games)
 
 PGN = PGN_analyzer("games.pgn")
 moves_list = PGN.moves_list
+
 result = PGN.analyze_all_moves()
 adv = PGN.adv
 
-main=Main("ThermalDust095",974,moves_list)
-main.mainloop(result,adv)
+rating_dict = {
+    "blitz": 0,
+    "rapid": 1,
+    "bullet": 2
+}
+
+main = Main(args.username, ratings[rating_dict[time_control]], moves_list)
+main.mainloop(result, adv)
